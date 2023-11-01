a12009d96dd2c4807fc09fff2105d8cc7cd62684
Gabriel Castro <72272327+OnlySig@users.noreply.github.com>
2023-11-01 11:58:00 -0300

    Terminando a primeira pagina

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,6 @@
 import { connectApi } from "./conexao.js"
+const produtos = await connectApi.getProdutos()
+const containerProdutos = document.querySelector(".produtos__container")
 const btnAmburger = document.querySelector("#btnAmburger")
 const mobileNav = document.querySelector(".mobile__ancors")
 const hamburger = document.querySelector(".hamburger")
@@ -10,10 +12,26 @@ btnAmburger.addEventListener("click", _ => {
     mobileNav.classList[1] === "off" ? hamburger.children[0].src = "/src/imgs/icon-close.svg" : hamburger.children[0].src = "/src/imgs/icon-hamburger.svg"  
 })
 
-console.log(connectApi.getProdutos)
 
 circulos.forEach(element => {
     element.addEventListener("click", e => {
         carrossel.style.backgroundImage = `url(/src/imgs/Photo${e.target.attributes[1].value}.png)`
     })
+})
+
+
+produtos.Produtos.forEach(element => {
+    console.log(element)
+    containerProdutos.innerHTML += `
+        <div class=${"produto__item "+element.id}>
+            <img src=${element.imageUrl} alt=${element.name} class="imagemProduto">
+            <div class="produto__infos">
+                <h3 class="subTitleProdutos">${element.name}</h3>
+                <div class="details">
+                    <span>${element.category}</span>
+                    <span>$${element.price}</span>
+                </div>
+            </div>
+        </div>
+    `
 })
